db8eb6084efc72f32d366640fcf26946fe377678
panda2134 <panda2134@DESKTOP-C1UVUG7>
2025-08-05 16:24:14 +0800

    添加项目文件。

---
+++ Core.cs
@@ -0,0 +1,15 @@
+﻿using MelonLoader;
+
+[assembly: MelonInfo(typeof(DontRuinMyAccount.Core), "DontRuinMyAccount", "1.0.0", "panda2134", null)]
+[assembly: MelonGame("sega-interactive", "Sinmai")]
+
+namespace DontRuinMyAccount
+{
+    public class Core : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("Initialized.");
+        }
+    }
+}
